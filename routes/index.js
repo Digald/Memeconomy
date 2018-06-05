df62e8fcbdea59b25957e352ed0b878324d4f2ae
Mark Alaniz <markalaniz000@gmail.com>
2018-06-04 22:00:35 -0500

    initial commit, still working on setup

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
