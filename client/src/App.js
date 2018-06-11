0d1d32839461311abc3bfddea49fc3a06e483fc1
Mark Alaniz <markalaniz000@gmail.com>
2018-06-10 20:43:14 -0500

    set up basic store and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import rootReducer from "./rootReducer";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import LandingPage from "./pages/LandingPage";
+
+const middleware = [logger, thunk];
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
