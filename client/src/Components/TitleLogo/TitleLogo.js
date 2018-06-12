14bc245443e4b951adf4910a279fa29da9e051f1
Digald <nosracer_gt@sbcglobal.net>
2018-06-12 17:20:47 -0500

    Leveled out title and logo and then changed background color the right shade

--- client/src/Components/TitleLogo/TitleLogo.js
+++ client/src/Components/TitleLogo/TitleLogo.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Level } from "reactbulma";
 import logo from "./logo.png";
 import "./TitleLogo.css";
 
 const TitleLogo = () => (
   <div className="TitleLogo">
-    <Link to="/">
-      <img className="main-logo" src={logo} alt="my logo" />
-      <h1 className="main-title">Memeconomy</h1>
-    </Link>
+    <Level>
+      <Link to="/">
+        <img className="main-logo" src={logo} alt="my logo" />
+        <h1 className="main-title">Memeconomy</h1>
+      </Link>
+    </Level>
   </div>
 );
 
