f59db57e6bd4fdc52e5048fd2b6d421fbe151b0a
Mark Alaniz <markalaniz000@gmail.com>
2018-06-23 21:41:03 -0500

    Added first icon to the screen. Just need to work on sizes, and background styles

--- client/src/Components/IndividualIcon/IndividualIcon.js
+++ client/src/Components/IndividualIcon/IndividualIcon.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './IndividualIcon.css';
+// import shopping from './shopping.svg';
 
 /*
 This component will be the individual SVG with white
@@ -8,7 +9,8 @@ circular background.
 
 const IndividualIcon = (props) => (
     <div className="column is-1">
-        <img src={props.icon1} alt={props.alt}/>
+        <img src={props.icon} alt={props.alt}/>
+        <p>{props.name}</p>
     </div>
 );
 
--- client/src/Components/MenuIcons/MenuIcons.js
+++ client/src/Components/MenuIcons/MenuIcons.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./MenuIcons.css";
 import IndividualIcon from "../IndividualIcon/IndividualIcon";
+import shopping from '../IndividualIcon/shopping.svg';
 
 /*
 This is the list of all menu settings that will be displayed
@@ -10,14 +11,14 @@ below the pages component.
 
 const MenuIcons = props => (
   <div className="columns MenuIcons">
-    <div className="column is-3"/>
+    <div className="column is-3" />
+    <IndividualIcon icon={shopping} alt="shopping not working" name="shopping bag"/>
     <IndividualIcon />
     <IndividualIcon />
     <IndividualIcon />
     <IndividualIcon />
     <IndividualIcon />
-    <IndividualIcon />
-    <div className="column is-3"/>
+    <div className="column is-3" />
   </div>
 );
 
