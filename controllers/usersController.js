e14c67fd9db18094de987ede92bf4e450d6170b3
Mark Alaniz <markalaniz000@gmail.com>
2018-07-21 00:21:16 -0500

    Added post routes for each login form and will need to start adding passport library next

---
+++ controllers/usersController.js
@@ -0,0 +1,7 @@
+exports.signup = async (req, res) => {
+    console.log(req.body);
+};
+
+exports.login = async (req, res) => {
+    console.log(req.body);
+}
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const pagesController = require('../controllers/pagesController');
+const usersController = require('../controllers/usersController');
 
 
-// API routes
+// API GET routes
 router.get('/api/pages', pagesController.getPages);
+
+// POST routes
+router.post('/signup', usersController.signup);
+router.post('/login', usersController.login);
+
 
 // serve html file for react
 router.use(function(req, res) {
